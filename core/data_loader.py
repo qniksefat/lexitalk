a46e211bc7f648b77e25b6a31659217bf0961840
Qasem Nick <qniksefat@gmail.com>
2024-01-22 19:41:36 +0330

    move metadata_extractor method, and update fill_nodes method

--- core/data_loader.py
+++ core/data_loader.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import List, Dict
 import os
 
 from llama_index import SimpleDirectoryReader
@@ -7,23 +7,7 @@ from llama_index.node_parser import SimpleNodeParser
 from core.config import INPUT_DIR
 
 
-def metadata_extractor(file_path: str) -> Dict:
-    """Get some handy metadate from filesystem.
-
-    Args:
-        file_path: str: file path in str
-    """
-    file_name = os.path.basename(file_path)
-    return {
-        "file_path": file_path,
-        "file_name": file_name,
-        "file_type": file_name.split(".")[-1],
-        "file_size": os.path.getsize(file_path),
-        "episode_number": int(file_name.split("_")[1])
-    }
-
-
-class EpisodeReader:
+class DatasetReader:
     def __init__(self, input_dir=INPUT_DIR):
         self.input_dir = input_dir
         self.docs = None
@@ -32,7 +16,7 @@ class EpisodeReader:
     def load_documents(self):
         """Load data from the specified directory."""
         reader = SimpleDirectoryReader(input_dir=self.input_dir, 
-                                       file_metadata=metadata_extractor)
+                                       file_metadata=self.metadata_extractor)
         self.docs = reader.load_data()
 
     def fill_documents(self):
@@ -45,8 +29,10 @@ class EpisodeReader:
         """Fill in the nodes attribute."""
         if not self.docs:
             raise ValueError("Documents have not been loaded. Call load_data first.")
+        
         parser = SimpleNodeParser.from_defaults(
             chunk_size=512,
+            chunk_overlap=128,
             include_metadata=True,
             include_prev_next_rel=True,
         )
@@ -57,11 +43,27 @@ class EpisodeReader:
         self.load_documents()
         self.fill_documents()
         self.fill_nodes()
+    
+    @staticmethod
+    def metadata_extractor(file_path: str) -> Dict:
+        """Get some handy metadate from filesystem.
+
+        Args:
+            file_path: str: file path in str
+        """
+        file_name = os.path.basename(file_path)
+        return {
+            "file_path": file_path,
+            "file_name": file_name,
+            "file_type": file_name.split(".")[-1],
+            "file_size": os.path.getsize(file_path),
+            "episode_number": int(file_name.split("_")[1])
+        }
 
 
 if __name__ == "__main__":
     # Example of using EpisodeReader
-    episode_reader = EpisodeReader()
+    episode_reader = DatasetReader()
     episode_reader.load_data()
 
     # Access documents and nodes attributes if needed
