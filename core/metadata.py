bbfcf3b82735742c4bc879a2025d53a3014579e0
Qasem Nick <qniksefat@gmail.com>
2024-01-23 01:36:26 +0330

    extract info from yt title. handles exceptions

--- core/metadata.py
+++ core/metadata.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class MetadataSingleton:
     _instance = None
 
@@ -10,14 +11,37 @@ class MetadataSingleton:
         return cls._instance
 
     def _generate_dataframe(self):
-        df = pd.read_csv("data/youtube_playlist/video_info.csv")
-        df["guest_names"] = df["title"].apply(lambda x: x.split(":")[0])
-        df["episode_number"] = df["title"].apply(lambda x: x.split("#")[1]).astype(int)
-        df["episode_title"] = df["title"].apply(lambda x: x.split("|")[0].strip())
-        df["episode_title"] = df["episode_title"].apply(lambda x: x.split(":")[1].strip())
-        df["url"] = "https://www.youtube.com/watch?v=" + df["video_id"]
+        df = pd.read_csv("data/youtube_playlist/videos.csv")
+        df["guest_names"] = df["title"].apply(self._extract_guest_names)
+        df["episode_number"] = df["title"].apply(self._extract_episode_number)
+        df["episode_title"] = df["title"].apply(self._extract_episode_title)
         df = df.set_index("episode_number")
         self.metadata_yt = df
+
+    def _extract_guest_names(self, title):
+        if ":" in title:
+            return title.split(":")[0]
+        else:
+            return ""
+
+    def _extract_episode_number(self, title):
+        # "Ask Me Anything (AMA)" episodes have "#" but not conventional episode numbers
+        if "AMA" not in title and "#" in title:
+            return int(title.split("#")[1])
+        else:
+            return None
+
+    def _extract_episode_title(self, title):
+        if "|" in title:
+            episode_title = title.split("|")[0].strip()
+            if ":" in episode_title:
+                return episode_title.split(":")[1].strip()
+            else:
+                return episode_title
+        else:
+            # Handle case where "|" is not present
+            return ""
+
 
 metadata_singleton = MetadataSingleton()
 metadata_yt = metadata_singleton.metadata_yt
