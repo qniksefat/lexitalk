e090ebd5d43f1099eced4eef7f8b13f25ceb8150
Qasem Nick <qniksefat@gmail.com>
2024-01-23 21:49:53 +0330

    Filled st_utils with new functions and UI improvements

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
 import streamlit as st
 import openai
+
+from core.st_utils import (
+    input_user_question, 
+    display_chat_messages, 
+    generate_assistant_response,
+    initialize_chat_messages,
+    make_sample_question_buttons,
+)
+from core.index_loader import load_vetor_index
+
 openai.api_key = st.secrets.openai_key
 
-from core.index_loader import load_vetor_index
-from core.st_utils import display_sources
+st.set_page_config(
+    page_title="LexiTalk",
+    page_icon="🎙️",
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items={
+        "About": ("LexiTalk guides you through [Lex Fridman Podcast](https://lexfridman.com/podcast) "
+                  "first 325 episodes taking transcripts from [here](https://karpathy.ai/lexicap/) unraveling perspectives.")
 
-st.set_page_config(page_title="LexiTalk", 
-                   page_icon="🎙️", layout="wide", 
-                   initial_sidebar_state="auto", menu_items=None)
-st.title("Perspectives Unleashed: Chat with Lex Fridman Guests 💬")
-st.write("LexiTalk guides you through [Lex Fridman Podcast](https://lexfridman.com/podcast), unraveling perspectives.")
+    },
+)
 
-# Initialize the chat messages history
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me about any topic!"}
-    ]
+st.title("Chat with Lex Fridman's Guests 💬")
 
 index = load_vetor_index(index_name=st.secrets["weaviate_index_name"])
 
 # Initialize the chat engine
-if "chat_engine" not in st.session_state.keys():
-    st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+if "chat_engine" not in st.session_state:
+    st.session_state.chat_engine = index.as_chat_engine(
+        similarity_top_k=5,
+        chat_mode="condense_question",
+        verbose=True
+    )
 
-# Prompt for user input and save to chat history
-if prompt := st.chat_input("Your question"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+initialize_chat_messages()
+st.write("\n")
+make_sample_question_buttons()
+input_user_question()
+display_chat_messages(st.session_state.messages)
 
-# Display the prior chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-# If last message is not from assistant, generate a new response
+# If the last message is not from the assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = st.session_state.chat_engine.chat(prompt)
-            st.write(response.response)
-            display_sources(response.source_nodes)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
+    generate_assistant_response(
+        st.session_state.chat_engine, st.session_state.messages[-1]["content"])
