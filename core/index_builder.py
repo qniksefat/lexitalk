72ff4372cbe4aa958a2b95eaaf3ad6c322e0446b
Qasem Nick <qniksefat@gmail.com>
2024-01-23 11:10:13 +0330

    Add Weaviate integration and index builder function

--- core/index_builder.py
+++ core/index_builder.py
@@ -9,6 +9,27 @@ from core.data_loader import DatasetReader
 
 
 def build_index(input_dir, index_name):
+    weaviate_client = build_weaviate_client()
+        
+    reader = DatasetReader(input_dir=input_dir)
+    reader.load_data()
+
+    vector_store = WeaviateVectorStore(weaviate_client=weaviate_client, 
+                                       index_name=index_name)
+    
+    # setting up the storage for the embeddings
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    
+    index = VectorStoreIndex(reader.nodes, 
+                             storage_context=storage_context)
+    
+    return index
+
+
+def build_weaviate_client():
+    
+    # maybe openai should be before storage_context?
+    openai.api_key = st.secrets["openai_key"]
 
     auth_client_secret=weaviate.AuthApiKey(api_key=st.secrets["weaviate_key"])
     
@@ -16,18 +37,19 @@ def build_index(input_dir, index_name):
             url=st.secrets["weaviate_url"],
             auth_client_secret=auth_client_secret,
             additional_headers={'X-OpenAI-Api-Key': st.secrets["openai_key"]})
-        
-    reader = DatasetReader(input_dir=input_dir)
-    reader.load_data()
-    nodes = reader.nodes
+            
+    return client
 
-    vector_store = WeaviateVectorStore(weaviate_client=client, index_name=index_name)
 
-    openai.api_key = st.secrets["openai_key"]
+def show_weaviate_schema(client):
+    """This will show a list of classes/indexes in the weaviate instance."""
+    import json
     
-    # setting up the storage for the embeddings
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-    
-    index = VectorStoreIndex(nodes, storage_context=storage_context)
+    schema = client.schema.get()
+    print(json.dumps(schema, indent=2))
 
-    return index
+
+def delete_weaviate_index(client, index_name):
+    """This will delete a class/index in the weaviate instance."""
+    client.schema.delete_class(index_name)
+    print(f"Deleted index {index_name}.")
