2aa78e8f0e1acb27a846a38fb92834e34f1296c9
Qasem Nick <qniksefat@gmail.com>
2024-02-13 13:28:23 +0700

    Break down index building and data preparation

--- core/index_builder.py
+++ core/index_builder.py
@@ -4,23 +4,36 @@ from llama_index.schema import MetadataMode
 from llama_index.vector_stores.utils import node_to_metadata_dict
 
 from core.data_loader import DatasetReader
-from core.embedding import load_or_generate_embeddings
+from core.embedding import EmbeddingManager
+from core.database import MongoDBClient
 from core.utils import hash_node
 
 
-def build_index(input_dir, 
-                mongodb_client, 
-                openai_client, 
-                index_name="vector_index",
-                remove_text=True    # Already saving text in node_content along with metadata
+def prepare_data_and_embeddings(
+                embedding_manager: EmbeddingManager,
+                input_dir=None,
+                text_nodes=None,
+                remove_text=True,
                 ):
-    reader = DatasetReader(input_dir=input_dir)
-    reader.load_data()
+    """Prepares the data for insertion into the MongoDB collection.
+    
+    If text_nodes is not None, it will use that to prepare the data.
+    Otherwise, it will use the input_dir to read the data.
+    
+    Generates or loads the embeddings for the text nodes.
+    
+    Already keeping text in node content, so remove_text=True by default.
+    """
+    assert (text_nodes is None) != (input_dir is None), "Either specify text_nodes or input_dir, not both"
+    if text_nodes is None:
+        reader = DatasetReader(input_dir=input_dir)
+        reader.load_data()
+        text_nodes = reader.nodes
 
-    hash_embeddings = load_or_generate_embeddings(reader.nodes, openai_client)
+    hash_embeddings = embedding_manager.load_or_generate_embeddings(text_nodes)
 
     data_to_insert = []
-    for node in tqdm(reader.nodes, desc="Pre-processing text nodes for MongoDB"):
+    for node in tqdm(text_nodes, desc="Pre-processing text nodes for MongoDB"):
         metadata = node_to_metadata_dict(node, remove_text=remove_text, flat_metadata=True)
         node_hash = hash_node(node)
         node_embedding = hash_embeddings[node_hash]
@@ -30,26 +43,32 @@ def build_index(input_dir,
             "embedding": node_embedding,
             "hash": node_hash,
         }
-        
         if not remove_text:
             text = node.get_content(metadata_mode=MetadataMode.NONE) or ""
             entry["text"] = text
         
         data_to_insert.append(entry)
+    return data_to_insert
 
+
+def build_index(data_to_insert: list,
+                mongodb_client: MongoDBClient,
+                index_name="vector_index",
+                ):
     mongodb_client.insert_many(data_to_insert)
 
 
 if __name__ == "__main__":
     from core.database import MongoDBClient
-    from core.embedding import OpenAIClient
+    from core.embedding import EmbeddingManager
 
     mongodb_client = MongoDBClient(database_name='default_db',
                                    collection_name='default_collection',
                                    index_name="vector_index")
 
-    openai_client = OpenAIClient()
-
-    build_index(input_dir="data/raw/sample",
-                mongodb_client=mongodb_client,
-                openai_client=openai_client)
+    embedding_manager = EmbeddingManager()
+    
+    data_to_insert = prepare_data_and_embeddings(embedding_manager, 
+                                                 input_dir="data/raw/sample")
+    
+    build_index(data_to_insert, mongodb_client=mongodb_client)
