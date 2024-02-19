e38ad02c396bf466be6ca99d135014062f3afb00
Qasem Nick <qniksefat@gmail.com>
2024-02-19 14:47:10 +0700

    Add function to retrieve public IP address for mongodb access

--- main.py
+++ main.py
@@ -20,31 +20,17 @@ st.info("Welcome aboard our AI-driven magic carpet! Journey through the fascinat
         icon="💡")
 
 
-username = "qniksefat"
-password = st.secrets["mongodb_password"]
-url = st.secrets["mongodb_url"]
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
+import requests
 
+def get_public_ip():
+    try:
+        response = requests.get('https://api.ipify.org')
+        if response.status_code == 200:
+            return response.text
+        else:
+            return "Failed to retrieve public IP"
+    except requests.RequestException as e:
+        return "Error: " + str(e)
 
-class MongoDBClient:
-    def __init__(self, connection_uri, database_name, collection_name):
-        self.client = MongoClient(connection_uri, server_api=ServerApi('1'))
-        self.db = self.client[database_name]
-        self.collection = self.db[collection_name]
-
-    def ping_connection(self):
-        try:
-            self.client.admin.command('ping')
-            print("Pinged your deployment. You successfully connected to MongoDB!")
-        except Exception as e:
-            print(e)
-
-connection_uri = (f"mongodb+srv://{username}:{password}@{url}/?retryWrites=true&w=majority")
-monngodb_client = MongoDBClient(connection_uri, 
-                             database_name = 'default_db',
-                             collection_name = 'default_collection')
-
-st.write(
-    monngodb_client.collection.find_one()
-)
+public_ip = get_public_ip()
+st.write("my public IP address is:", public_ip)
