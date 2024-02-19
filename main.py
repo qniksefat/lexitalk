b4bef76d5eee72ea1b84354517577d66939bf89b
Qasem Nick <qniksefat@gmail.com>
2024-02-19 14:51:09 +0700

    Test MongoDB client and connection network access

--- main.py
+++ main.py
@@ -34,3 +34,33 @@ def get_public_ip():
 
 public_ip = get_public_ip()
 st.write("my public IP address is:", public_ip)
+
+
+username = "qniksefat"
+password = st.secrets["mongodb_password"]
+url = st.secrets["mongodb_url"]
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+
+class MongoDBClient:
+    def __init__(self, connection_uri, database_name, collection_name):
+        self.client = MongoClient(connection_uri, server_api=ServerApi('1'))
+        self.db = self.client[database_name]
+        self.collection = self.db[collection_name]
+
+    def ping_connection(self):
+        try:
+            self.client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+
+connection_uri = (f"mongodb+srv://{username}:{password}@{url}/?retryWrites=true&w=majority")
+monngodb_client = MongoDBClient(connection_uri, 
+                             database_name = 'default_db',
+                             collection_name = 'default_collection')
+
+st.write(
+    monngodb_client.collection.find_one()
+)
